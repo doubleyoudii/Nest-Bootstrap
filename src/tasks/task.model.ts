8a7c7a6ee55bb8aa836f1dc6fe062c67fa897b3f
William Ducta <wducta@yondu.com>
2022-02-23 23:21:11 +0800

    CHORE: Create model for Task then import it

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,13 @@
+// In this example, we will use interface but soon we will change into classes
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+}
+
+enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
