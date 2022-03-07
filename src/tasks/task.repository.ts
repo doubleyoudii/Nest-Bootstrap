06a8b30cfae16085d87bbe3ceebd20fce88c8880
William Ducta <wducta@yondu.com>
2022-03-07 15:36:09 +0800

    CHORE: Transfer logic specific to repository file

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,20 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum.';
 import { Task } from './task.entity';
 
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+  async createTask(createTask: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTask;
+
+    const task = {
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    const createdTask = this.create(task);
+    await this.save(createdTask);
+    return createdTask;
+  }
+}
