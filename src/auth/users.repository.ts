6cbabfc00542f3e78005f388d957f55ef4c5d4e5
William Ducta <wducta@yondu.com>
2022-03-13 14:10:59 +0800

    CHORE: Handle Duplicate columns in typeorm

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { UserCreadentialsDTO } from './dto/user-credentials.dto';
 import { User } from './user.entity';
@@ -7,6 +11,15 @@ export class UserRepository extends Repository<User> {
   async createUser(userCreds: UserCreadentialsDTO): Promise<void> {
     const { username, password } = userCreds;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username Already Exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
