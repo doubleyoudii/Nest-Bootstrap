f0cebf553c53625d90763708fb4e309dcfbb6c41
William Ducta <wducta@yondu.com>
2022-03-14 14:49:48 +0800

    CHORE: add jwt for accesstoken

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signin(@Body() body: SignInDTO): Promise<string> {
+  signin(@Body() body: SignInDTO): Promise<{ accessToken: string }> {
     return this.authService.signin(body);
   }
 }
