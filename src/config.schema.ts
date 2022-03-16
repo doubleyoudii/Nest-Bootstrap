b4b6f4b1e0fce062f1b6106661aa9b55deaeb2d8
William Ducta <wducta@yondu.com>
2022-03-16 22:29:43 +0800

    CHORE: Apply Schema validation for Config

---
+++ src/config.schema.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi';
+
+export const ConfigValidation = Joi.object({
+  PORT: Joi.number().default(3000),
+  STAGE: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_HOST: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+});
